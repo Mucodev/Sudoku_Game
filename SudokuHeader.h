3a270e5827c2f20b614eec8f30b541d21b4e5e59
Mucodev <82687664+Mucodev@users.noreply.github.com>
2022-12-24 17:02:13 +0300

    Update SudokuHeader.h

--- SudokuHeader.h
+++ SudokuHeader.h
@@ -12,16 +12,16 @@ class SudokuGame
 {
 private:
     int sudokuArray[9][9] =
-        {
-            {0, 0, 6, 5, 0, 8, 4, 0, 0},
-            {5, 2, 0, 0, 0, 1, 0, 0, 0},
-            {0, 7, 8, 0, 0, 0, 0, 0, 1},
-            {0, 0, 4, 0, 1, 0, 0, 8, 0},
-            {9, 0, 0, 8, 2, 3, 0, 0, 5},
-            {0, 5, 0, 0, 9, 0, 6, 0, 0},
-            {1, 3, 0, 0, 0, 0, 2, 5, 0},
-            {0, 0, 0, 0, 0, 0, 0, 7, 4},
-            {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+    {
+        {0, 0, 6, 5, 0, 8, 4, 0, 0},
+        {5, 2, 0, 0, 0, 1, 0, 0, 0},
+        {0, 7, 8, 0, 0, 0, 0, 0, 1},
+        {0, 0, 4, 0, 1, 0, 0, 8, 0},
+        {9, 0, 0, 8, 2, 3, 0, 0, 5},
+        {0, 5, 0, 0, 9, 0, 6, 0, 0},
+        {1, 3, 0, 0, 0, 0, 2, 5, 0},
+        {0, 0, 0, 0, 0, 0, 0, 7, 4},
+        {0, 0, 5, 2, 0, 6, 3, 0, 0} };
     /*
     int[][] grid =
     {
@@ -47,7 +47,6 @@ private:
      {0, 0, 7, 2, 0, 6, 9, 0, 0},
      {0, 4, 0, 5, 0, 8, 0, 7, 0},
     };
-
     int[][] grid =
     {
      {8, 0, 0, 0, 0, 7, 0, 0, 0},
@@ -60,25 +59,25 @@ private:
      {0, 0, 0, 0, 5, 0, 2, 8, 0},
      {0, 0, 0, 8, 0, 0, 0, 0, 3},
     };
-
     */
 public:
     SudokuGame()
     {
         cout << setw(50) << "Welcome to Sudoku Game" << endl
-             << setw(40)
-             << "Rules" << endl
-             << endl
-             << setw(20) << "1. Use Numbers 1-9" << endl
-             << setw(59) << "2. Don't Repeat Any Numbers(each row, column and square) " << endl;
+            << setw(40)
+            << "Rules" << endl
+            << endl
+            << setw(20) << "1. Use Numbers 1-9" << endl
+            << setw(59) << "2. Don't Repeat Any Numbers(each row, column and square) " << endl;
         cout << "Press any key to continue...";
         (void)_getch();
+        cout << endl;
     }
 
     void displayGame() // Alperen
     {
         cout << "#   1 2 3   4 5 6   7 8 9" << endl
-             << "+ | - - - + - - - + - - -" << endl;
+            << "+ | - - - + - - - + - - -" << endl;
 
         for (int i = 0; i < 9; i++)
         {
@@ -119,9 +118,9 @@ public:
     {
         for (int i = 0; i < 9; i++)
         {
-            if (tnumber == sudokuArray[i][column])
+
+            if (sudokuArray[i][column-1] == tnumber)
             {
-                cout << "False" << endl;
                 return false;
             }
         }
@@ -132,9 +131,8 @@ public:
     {
         for (int i = 0; i < 9; i++)
         {
-            if (tnumber == sudokuArray[row][i])
+            if (tnumber == sudokuArray[row-1][i])
             {
-                cout << "False" << endl;
                 return false;
             }
         }
@@ -191,12 +189,18 @@ public:
 
     void inputs(int row, int column, int number)     //Onur
     {
-	if(rowControl(row, number) && columnControl(column, number) && squareControl(row, column, number))
-	{
-		sudokuArray[row][column] = number;
-	}
+        if (rowControl(row, number))
+        {
+            if (columnControl(column, number))
+            {
+                if (squareControl(row, column, number))
+                {
+                    sudokuArray[row - 1][column - 1] = number;
+                }
+            }
+        }
     }
-    
+
     void showSolution()      // Mücahit
     {
         for (int r = 0; r < 9; r++)
